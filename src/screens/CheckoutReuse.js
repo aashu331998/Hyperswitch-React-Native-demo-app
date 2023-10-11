8f824032b7cb2900d7c27c1e7a471e65c753612c
Aashutosh Bansal <aashutosh.bansal@juspay.in>
2023-10-11 19:17:17 +0530

    version updated

--- src/screens/CheckoutReuse.js
+++ src/screens/CheckoutReuse.js
@@ -14,12 +14,13 @@ const CheckoutReuse = ({client_secret, ephemeral_key, price}) => {
     };
     console.log('reusable => ', client_secret, ephemeral_key);
     const {error} = await initPaymentSheet({
-      customerId: ephemeral_key.customer_id,
-      customerEphemeralKeySecret: ephemeral_key.secret,
+      // customerId: ephemeral_key.customer_id,
+      // customerEphemeralKeySecret: ephemeral_key.secret,
       merchantDisplayName: 'Example',
       paymentIntentClientSecret: client_secret,
       googlePay: googlePay,
-      style: 'AlwaysDark',
+      // style: 'AlwaysDark',
+      // appearance: {locale: 'he'},
     });
     if (!error) {
     } else {
@@ -31,14 +32,42 @@ const CheckoutReuse = ({client_secret, ephemeral_key, price}) => {
     initializePaymentSheet();
   }, []);
 
+  // presentPaymentSheet() response
+  // {"error": {"code": "cancelled", "message": "cancelled"}}
+  // {"paymentOption": {"image": "succeeded", "label": "succeeded"}}
+  // {"error": {"code": "failed", "message": "failed"}}
+  // {"paymentOption": {"image": "requires_capture", "label": "requires_capture"}}
+
   const openPaymentSheet = async () => {
     const res = await presentPaymentSheet();
     console.log('presentPaymentSheet response: ', res);
-    const {error} = res;
+    const {error, paymentOption} = res;
     if (error) {
-      Alert.alert(`Error code: ${error.code}`, error.message);
+      switch (error.code) {
+        case 'cancelled':
+          Alert.alert('cancelled', `PaymentSheet was closed`);
+          break;
+        case 'failed':
+          Alert.alert('failed', `Payment failed`);
+          break;
+        default:
+          Alert.alert('Something went wrong', 'Please check the integration');
+          break;
+      }
+    } else if (paymentOption) {
+      switch (paymentOption.label) {
+        case 'succeeded':
+          Alert.alert('succeeded', `Your order is succeeded`);
+          break;
+        case 'requires_capture':
+          Alert.alert('requires_capture', `Your order is requires_capture`);
+          break;
+        default:
+          Alert.alert('succeeded', 'Please check the integration');
+          break;
+      }
     } else {
-      Alert.alert('Success', 'Your order is confirmed!');
+      Alert.alert('Something went wrong', 'Please check the integration');
     }
     //to reFetch clientSecret
     //initializePaymentSheet(price);
